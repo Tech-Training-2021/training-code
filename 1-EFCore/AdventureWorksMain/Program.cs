681d0e2a147cae260e99bab603d316b1d71bddfe
kaurrevature <33356218+kaurrevature@users.noreply.github.com>
2021-02-08 11:24:34 +0530

    Dotnet day 1- Hello World and EFCore Data First

---
+++ 1-EFCore/AdventureWorksMain/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using AdventureWorksMain.Entities;
+using System.Linq;
+
+namespace AdventureWorksMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         /*  int[] numbers=new int[]{1,2,3,4,5,6,7,8,9,10};
+           foreach (var num in numbers)
+           {
+               if(num%2 == 0)
+                Console.Write($"{num} ");// string interpolation
+           }*/
+        AdventureWorksDbContext dbContext=new AdventureWorksDbContext();
+        var customers= dbContext.Customers;
+            foreach (var customer in customers)
+            {
+                if(customer.CustomerId<11)
+                    Console.WriteLine($"{customer.FirstName} {customer.LastName}");
+            }
+        }
+    }
+}
